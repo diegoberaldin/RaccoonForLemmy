38c6046254f16176064de6facf9da461dfab99b9
Diego Beraldin <diego.beraldin@gmail.com>
2024-03-05 21:39:23 +0100

    update business logic

--- unit/ban/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/unit/ban/BanUserMviModel.kt
+++ unit/ban/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/unit/ban/BanUserMviModel.kt
@@ -12,10 +12,17 @@ interface BanUserMviModel :
 
     sealed interface Intent {
         data class SetText(val value: String) : Intent
+        data class ChangePermanent(val value: Boolean) : Intent
+        data class ChangeRemoveData(val value: Boolean) : Intent
+        data class ChangeDays(val value: Int) : Intent
         data object Submit : Intent
     }
 
     data class UiState(
+        val permanent: Boolean = true,
+        val days: Int? = null,
+        val targetBanValue: Boolean = false,
+        val removeData: Boolean = false,
         val text: String = "",
         val textError: ValidationError? = null,
         val loading: Boolean = false,
