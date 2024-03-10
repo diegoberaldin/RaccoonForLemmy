38c7bf3a42053110ae1af4a0528bb5068fab8a77
Diego Beraldin <diego.beraldin@gmail.com>
2024-03-10 16:12:56 +0100

    update login, logout and switch account use cases

--- domain/identity/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/domain/identity/di/IdentityModule.kt
+++ domain/identity/src/commonMain/kotlin/com/github/diegoberaldin/raccoonforlemmy/domain/identity/di/IdentityModule.kt
@@ -44,6 +44,7 @@ val coreIdentityModule = module {
             accountRepository = get(),
             settingsRepository = get(),
             siteRepository = get(),
+            communitySortRepository = get(),
         )
     }
     single<LogoutUseCase> {
@@ -53,6 +54,7 @@ val coreIdentityModule = module {
             notificationCenter = get(),
             settingsRepository = get(),
             contentResetCoordinator = get(),
+            communitySortRepository = get(),
         )
     }
     single<SwitchAccountUseCase> {
@@ -62,6 +64,7 @@ val coreIdentityModule = module {
             settingsRepository = get(),
             serviceProvider = get(named("default")),
             notificationCenter = get(),
+            communitySortRepository = get(),
         )
     }
     single<DeleteAccountUseCase> {
