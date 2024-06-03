2f95a372837118e8051e49867dc4667cdeab911d
Diego Beraldin <diego.beraldin@gmail.com>
2024-06-03 21:10:39 +0200

    remove unnecessary dependencies

--- core/architecture/build.gradle.kts
+++ core/architecture/build.gradle.kts
@@ -3,8 +3,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.compose)
-    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.detekt)
 }
 
@@ -30,13 +28,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
+                implementation(libs.kotlinx.coroutines)
             }
         }
     }
