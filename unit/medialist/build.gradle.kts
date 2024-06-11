17de78a60e2107240ba2b4d40ad90ef5d183e525
Diego Beraldin <diego.beraldin@gmail.com>
2024-06-11 21:42:42 +0200

    setup module and DI

--- unit/medialist/build.gradle.kts
+++ unit/medialist/build.gradle.kts
@@ -22,7 +22,7 @@ kotlin {
         iosSimulatorArm64(),
     ).forEach {
         it.binaries.framework {
-            baseName = "feature.settings"
+            baseName = "unit.medialist"
             isStatic = true
         }
     }
@@ -32,44 +32,30 @@ kotlin {
             dependencies {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
+                implementation(compose.material)
                 implementation(compose.material3)
                 implementation(compose.materialIconsExtended)
 
                 implementation(libs.koin.core)
-                implementation(libs.lyricist)
                 implementation(libs.voyager.navigator)
                 implementation(libs.voyager.screenmodel)
                 implementation(libs.voyager.koin)
-                implementation(libs.voyager.tab)
-                implementation(libs.voyager.bottomsheet)
 
                 implementation(projects.core.appearance)
                 implementation(projects.core.architecture)
                 implementation(projects.core.commonui.components)
-                implementation(projects.core.commonui.detailopenerApi)
                 implementation(projects.core.commonui.lemmyui)
-                implementation(projects.core.commonui.modals)
                 implementation(projects.core.l10n)
                 implementation(projects.core.navigation)
-                implementation(projects.core.notifications)
                 implementation(projects.core.persistence)
-                implementation(projects.core.preferences)
                 implementation(projects.core.utils)
 
-                implementation(projects.domain.lemmy.data)
-                implementation(projects.domain.lemmy.repository)
                 implementation(projects.domain.identity)
+                implementation(projects.domain.lemmy.data)
+                implementation(projects.domain.lemmy.pagination)
+                implementation(projects.domain.lemmy.repository)
 
-                implementation(projects.unit.web)
-                implementation(projects.unit.about)
-                implementation(projects.unit.accountsettings)
-                implementation(projects.unit.manageban)
-                implementation(projects.unit.choosecolor)
-                implementation(projects.unit.choosefont)
-                implementation(projects.unit.configureswipeactions)
-                implementation(projects.unit.configurecontentview)
-                implementation(projects.unit.filteredcontents)
-                implementation(projects.unit.medialist)
+                implementation(projects.unit.zoomableimage)
             }
         }
         val commonTest by getting {
@@ -81,7 +67,7 @@ kotlin {
 }
 
 android {
-    namespace = "com.github.diegoberaldin.raccoonforlemmy.feature.settings"
+    namespace = "com.github.diegoberaldin.raccoonforlemmy.unit.medialist"
     compileSdk = libs.versions.android.targetSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
